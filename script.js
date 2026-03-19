9955a9f1869fd22c8c8b5b187b2971fe7a0ef020
unknown <mansi.knp19@gmail.com>
2026-03-19 21:57:17 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let inp=document.querySelector("input");
+let span=document.querySelector("span");
+
+// inp.addEventListener("input",()=>{
+//     span.textContent=(inp.value.length);
+// });
+
+inp.addEventListener("input",()=>{
+    let left = 20-(inp.value.length);
+    span.textContent=left;
+    if(left<0) span.style.color="red";
+    else span.style.color="black";
+});
